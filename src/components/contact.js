ceccc1572cf67b6d926ddbf16d776c044ec35cc7
ranvir <ranveerbhatti2106@gmail.com>
2024-12-19 17:09:44 +0530

    email

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,7 +11,7 @@ export default function Contact() {
                 <p>I'm currently looking for opporunities</p>
                 <p>My Inbox is always Open So feel free to Contact Me</p>
                 <p style={{ color: "#00FFF5",marginBottom: "1rem" }}>MY Email - ranveerbhatti2106@gmail</p>
-                <a id='call' href="mailto:ranveerbhatti2106@gmail.com" className='showMore contactbtn fadeUp'>Say Hi</a>
+                <a id='call' href="mailto:ranvir.s.bhatti@gmail.com" className='showMore contactbtn fadeUp'>Say Hi</a>
                 </Block>
                     
             </div>
