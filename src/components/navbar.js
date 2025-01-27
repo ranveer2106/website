9b7b0299285337677cedc9a87d0a124ca9a15730
Ranvir Singh Bhatti <ranvirsingh.bhatti2020@vitstudent.ac.in>
2025-01-27 17:26:08 +0530

    changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -21,7 +21,7 @@ export default function Navbar(props) {
       <div id='options' className={` ${isActive ? "NotActive" : "active"}`}>
         <ul className={`options ${isActive ? "NotActive" : "active"}`}>
           <li className='items fadeUp'><Link className='link navl' to="/">Home</Link></li>
-          <li className='items fadeUp' ><a className='link navl' rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1UWn5GrtpHcXtywX3iQMX81AsVE_mvfDU/view?usp=sharing">Resume</a></li>
+          <li className='items fadeUp' ><a className='link navl' rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1gyujKEgreE6x-eZzWQYjwxc7xTMPG6Dv/view?usp=sharing">Resume</a></li>
           <li className='items fadeUp'><Link className='link navl' to="/projects">Projects</Link></li>
           <li className='items fadeUp'><Link className='link navl' to="/contact">Contact</Link></li>
         </ul>
